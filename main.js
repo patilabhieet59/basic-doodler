5b62cfde1c06f0023a1f03c5f0841715564319dd
Abhijeet Patil <a.patil@t-systems.com>
2020-11-12 18:29:37 +0530

    doodler movement

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 document.addEventListener('DOMContentLoaded',() => {
     const grid = document.querySelector('.grid');
+    let doodler;
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 500
     let platformSpacing = 120
     let platforms = []
+    let gameover = false
     
+    class Doodler {
+        constructor() {
+            this.left = platforms[0].left + 10;
+            this.bottom = platforms[0].bottom + 15;
+            let visual = document.createElement('div');
+            this.visual = visual;
+            visual.classList.add('doodler');
+            visual.style.left = this.left + 'px';
+            visual.style.bottom = this.bottom + 'px';
+            grid.appendChild(visual);
+        }
+    }
     
     function createDoodler() {
-        const doodler = document.createElement('div');
-        this.left = doodlerLeftSpace;
-        this.bottom = doodlerBottomSpace;
-        doodler.classList.add('doodler');
-        doodler.style.left = doodlerLeftSpace + 'px';
-        doodler.style.bottom = doodlerBottomSpace + 'px';
-        grid.appendChild(doodler);
+        doodler = new Doodler();
+       
     }
-    createDoodler();
+    
 
     class Platform {
         constructor(bottom){
@@ -33,21 +42,50 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     function createPlatform() {
-        for(var i=0;i<5;i++) {
+        for(var i=1;i<=5;i++) {
             let bottom = platformSpacing * i;
             let platform = new Platform(bottom);
         }
         
     }
-    createPlatform();
+   
 
     function movePlatforms() {
         setInterval(()=>{
+            let modify = false;
             platforms.forEach((p) => {
                 p.bottom -= 5;
                 p.visual.style.bottom = p.bottom + 'px';
+                if(p.bottom < 0) {
+                    modify = true;
+                }
             })
+            if(modify) {
+                let visual = platforms[0].visual;
+                visual.classList.remove('platform');
+                platforms.shift();
+                let bottom = platformSpacing * 5;
+                let platform = new Platform(bottom);
+                modify = false;
+            }
         },300)
     }
-    movePlatforms();
+    
+    function control(e){
+        if(e.key === 'ArrowUp') {
+            console.log(doodler.bottom);
+            doodler.bottom += 50;
+            doodler.visual.style.bottom = doodler.bottom + 'px';
+        }
+    }
+
+    function start(){
+        if(!gameover) {
+            createPlatform();
+            createDoodler();
+            movePlatforms();
+            document.addEventListener('keyup',control);
+        }
+    }
+    start();
 })
